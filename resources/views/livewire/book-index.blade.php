af0acb8e28e637ec0dc342915acbfedb5ca90659
Alex Garrett-Smith <alex@codecourse.com>
2023-10-18 09:55:08 +0100

    Episode 06

--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,5 +1,15 @@
-<div class="space-y-2">
-    @foreach ($books as $book)
-        <livewire:book-item :book="$book" wire:key="$book->id" />
-    @endforeach
+<div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+            <form wire:submit.prevent="createBook">
+                <input type="text" id="title" wire:model="title">
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+    <div class="space-y-2">
+        @foreach ($books as $book)
+            <livewire:book-item :book="$book" wire:key="$book->id" />
+        @endforeach
+    </div>
 </div>
