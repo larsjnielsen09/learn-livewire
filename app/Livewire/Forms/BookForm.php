6e8ea872c074de76f48e5fba6a4138d374824678
Alex Garrett-Smith <alex@codecourse.com>
2023-10-18 10:29:31 +0100

    Episode 08

--- app/Livewire/Forms/BookForm.php
+++ app/Livewire/Forms/BookForm.php
@@ -7,8 +7,11 @@ use Livewire\Form;
 
 class BookForm extends Form
 {
+    #[Rule('required', message: ':attribute is required', as: 'Title of the book')]
+    // #[Rule('min:20')]
     public string $title = '';
 
+    #[Rule('required', message: ':attribute is required', as: 'Author of the book')]
     public string $author = '';
 
     public function create()
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,15 +1,21 @@
 <div class="space-y-2">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900">
-            <form wire:submit.prevent="submit" class="flex items-center space-x-3">
+            <form wire:submit.prevent="submit" class="flex items-start space-x-3">
                 <div class="grow">
                     <label for="title" class="sr-only">Book title</label>
                     <input type="text" id="title" wire:model="form.title" placeholder="Book title" class="w-full border border-slate-300 rounded-lg">
+                    @error('form.title')
+                        <div class="mt-1 text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="grow">
                     <label for="title" class="sr-only">Book author</label>
                     <input type="text" id="author" wire:model="form.author" placeholder="Book author" class="w-full border border-slate-300 rounded-lg">
+                    @error('form.author')
+                        <div class="mt-1 text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-lg font-medium">Submit</button>
