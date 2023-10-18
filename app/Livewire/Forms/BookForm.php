160ee23b88d57213450c1c10c02d9c08d9bf0b57
Alex Garrett-Smith <alex@codecourse.com>
2023-10-18 10:13:20 +0100

    Episode 07

---
+++ app/Livewire/Forms/BookForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Rule;
+use Livewire\Form;
+
+class BookForm extends Form
+{
+    public string $title = '';
+
+    public string $author = '';
+
+    public function create()
+    {
+        auth()->user()->books()->create([
+            'title' => $this->title,
+            'author' => $this->author
+        ]);
+
+        $this->reset();
+    }
+}
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,15 +1,24 @@
-<div>
+<div class="space-y-2">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900">
-            <form wire:submit.prevent="createBook">
-                <input type="text" id="title" wire:model="title">
-                <button type="submit">Submit</button>
+            <form wire:submit.prevent="submit" class="flex items-center space-x-3">
+                <div class="grow">
+                    <label for="title" class="sr-only">Book title</label>
+                    <input type="text" id="title" wire:model="form.title" placeholder="Book title" class="w-full border border-slate-300 rounded-lg">
+                </div>
+
+                <div class="grow">
+                    <label for="title" class="sr-only">Book author</label>
+                    <input type="text" id="author" wire:model="form.author" placeholder="Book author" class="w-full border border-slate-300 rounded-lg">
+                </div>
+
+                <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-lg font-medium">Submit</button>
             </form>
         </div>
     </div>
     <div class="space-y-2">
         @foreach ($books as $book)
-            <livewire:book-item :book="$book" wire:key="$book->id" />
+            <livewire:book-item :book="$book" wire:key="{{ $book->id }}" />
         @endforeach
     </div>
 </div>
